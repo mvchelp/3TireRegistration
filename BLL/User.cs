958db25e3d4c65b495d93ebadad64b5b3a8fbfa0
rahul868 <rahul.raval868@gmail.com>
2016-08-03 20:52:42 +0530

    Add files via upload

---
+++ BLL/User.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class User
+    {
+        private int _UserId;
+        private string _FirstName;
+        private string _LastName;
+        private int _Gender;
+        private string _Email;
+        private string _Password;
+
+        public int UserId
+        {
+            get { return _UserId; }
+            set { _UserId = value; }
+        }
+        public string FirstName
+        {
+            get { return _FirstName; }
+            set { _FirstName = value; }
+        }
+        public string LastName
+        {
+            get { return _LastName; }
+            set { _LastName = value; }
+        }
+        public int Gender
+        {
+            get { return _Gender; }
+            set { _Gender = value; }
+        }
+        public string Email
+        {
+            get { return _Email; }
+            set { _Email = value; }
+        }
+        public string Password
+        {
+            get { return _Password; }
+            set { _Password = value; }
+        }
+    }
+}
---
+++ BLL/UserFactory.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DLL;
+using System.Data;
+
+namespace BLL
+{
+    public class UserFactory
+    {
+        public object ValidateUser(User objUser)
+        {
+            CLConn objCon = new CLConn();
+            object result = 0;
+
+            try
+            {
+                SqlParameter[] parameters = {  
+                                                new SqlParameter("@Email",SqlDbType.VarChar,50){Value=objUser.Email},
+                                                new SqlParameter("@Password",SqlDbType.VarChar,16){Value=objUser.Password}
+                                            };
+                result = objCon.ExcuteScalre("ValidateUser", parameters);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("something wrong");
+            }
+
+            return result;
+        }
+
+        public object ForgetEmail(string Email)
+        {
+            CLConn objCon = new CLConn();
+            object result = 0;
+
+            try
+            {
+                SqlParameter[] parameters = {  
+                                                new SqlParameter("@Email",SqlDbType.VarChar,50){Value=Email}
+                                            };
+                result = objCon.ExcuteScalre("GetUser", parameters);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("something wrong");
+            }
+
+            return result;
+        }
+
+        public object SaveUser(User objUser)
+        {
+            CLConn objCon = new CLConn();
+            object result = 0;
+
+            try
+            {
+                SqlParameter[] parameters = {  
+                                                new SqlParameter("@FirstName",SqlDbType.VarChar,20){Value=objUser.FirstName},
+                                                new SqlParameter("@LastName",SqlDbType.VarChar,20){Value=objUser.LastName},
+                                                new SqlParameter("@Gender",SqlDbType.Int,1){Value=objUser.Gender},
+                                                new SqlParameter("@Email",SqlDbType.VarChar,50){Value=objUser.Email},
+                                                new SqlParameter("@Password",SqlDbType.VarChar,16){Value=objUser.Password},
+                                            };
+                result = objCon.ExcuteScalre("AddUser", parameters);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("something wrong");
+            }
+
+            return result;
+        }
+
+        public object ResetPassword(User objUser)
+        {
+            CLConn objCon = new CLConn();
+            object result = 0;
+
+            try
+            {
+                SqlParameter[] parameters = {  
+                                                new SqlParameter("@UserId",SqlDbType.VarChar,50){Value=objUser.UserId},
+                                                new SqlParameter("@Password",SqlDbType.VarChar,16){Value=objUser.Password}
+                                            };
+                result = objCon.ExcuteScalre("ResetPassword", parameters);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("something wrong");
+            }
+
+            return result;
+        }
+    }
+}
